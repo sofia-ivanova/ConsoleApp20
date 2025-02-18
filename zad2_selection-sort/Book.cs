0521e7feb559b35abde3fab1f6ca4b1fea49bc4e
Sofia Ivanova <sofia.iivanova@gmail.com>
2025-02-18 10:57:16 +0200

    .

---
+++ zad2_selection-sort/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2_selection_sort
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public Book(string title, string author)
+        {
+            Title = title;
+            Author = author;
+        }
+        public override string ToString()
+        {
+            return $"{Title} - {Author}";
+        }
+    }
+}
---
+++ zad2_selection-sort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2_selection_sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book[] books =
+        {
+            new Book("Метро 2033", "Дмитрий Глуховски"),
+            new Book("1984", "Джордж Оруел"),
+            new Book("Престъпление и наказание", "Ф. Достоевски"),
+            new Book("Алиса в страната на чудесата", "Луис Карол")
+        };
+            int n = books.Length;
+            for (int i = 0; i < n-1; i++)
+            {
+                int minIndex = i;
+                for (int j = i+1; j < n; j++)
+                {
+                    if (books[j].Title[0] < books[minIndex].Title[0])
+                    {
+                        minIndex = j;
+                    }
+                    (books[i], books[minIndex]) = (books[minIndex], books[i]);
+                }
+            }
+            foreach (var book in books)
+            {
+                Console.WriteLine(book);
+            }
+            Console.ReadKey();
+        }
+    }
+}
